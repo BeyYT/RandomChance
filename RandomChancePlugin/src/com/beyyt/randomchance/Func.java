5a7c1ff404d62865e029c70ad4415a0a561a9b89
BeyYT <60990459+BeyYT@users.noreply.github.com>
2022-08-02 20:01:48 -0600

    Added Files

---
+++ RandomChancePlugin/src/com/beyyt/randomchance/Func.java
@@ -0,0 +1,58 @@
+package com.beyyt.randomchance;
+
+import java.util.ArrayList;
+
+import org.bukkit.Material;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+
+// enchantment import
+// import org.bukkit.enchantments.Enchantment;
+
+// colored text import 
+// import org.bukkit.ChatColor;
+
+
+public class Func {
+	public void test(Entity killer) {
+
+		// Gets Player Inventory
+		PlayerInventory invent = ((Player) killer).getInventory();
+			  
+		// Creates Item and gets item meta.
+		ItemStack item = new ItemStack(Material.GRASS_BLOCK);
+		ItemMeta item_meta = item.getItemMeta();
+			  
+		// sets display name of item
+		item_meta.setDisplayName("Change Item Name, Also You Can Use Colored / Bolded / Italic / Underlined Text with ChatColor.");
+			  
+		// you can add any enchantment you want.
+		// just uncomment the line below.
+		
+		//meta_core.addEnchant(Enchantment.DAMAGE_ALL, 1, true);
+			  
+		// lore list (secretly string list)
+		ArrayList<String> loreList = new ArrayList<String>();
+			  
+		// Actual Lore
+		// To Add lore, do loreList.add("String Goes Here");
+		loreList.add("");
+			
+			  
+		// Sets item Lore and meta to the item
+		item_meta.setLore(loreList);
+		item.setItemMeta(item_meta);
+			  
+		// Adds item to inventory.
+		invent.addItem(item);
+	}
+	
+}
+	
+	
+	
+
---
+++ RandomChancePlugin/src/com/beyyt/randomchance/RandomChance.java
@@ -0,0 +1,57 @@
+package com.beyyt.randomchance;
+import java.util.Random;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RandomChance extends JavaPlugin {
+	
+	// does thing when plugin is enabled
+    @Override
+    public void onEnable() {
+    	System.out.println("Booting Up...");
+    	getServer().getPluginManager().registerEvents(new Checks(), this);
+    }
+    // does thing when plugin is disabled
+    @Override
+    public void onDisable() {
+    	// Unless You Plan To Do Anything here, leave this empty
+    }
+    
+    // drop chance thing
+    public class Checks implements Listener {
+	    @EventHandler
+	    // on mob death
+	    public void onDeath(EntityDeathEvent e){
+	      // Imports Func.java
+	      Func drops = new Func();
+	      
+	      // Gets Dead Mob Entity.
+	      
+	      // Use if you are checking if a specific mod is killed.
+	      // for ex: if(chance == some_int && instanceof Zombie)
+	      
+	      // Entity mob = e.getEntity();
+	      
+	      // Gets The Killer (Player)
+	      Entity killer = e.getEntity().getKiller();
+	    
+	      // if killer is player
+	      if (killer instanceof Player){
+	    	  // not gambling 100%
+	    	  Random r = new Random();
+	    	  
+	    	  // 1 in 10 chance to drop
+	    	  int test = r.nextInt(10);
+
+	    	  // Dog Core Drop
+	    	  if(test == 5) {
+	    		  drops.test(killer);
+	    	  }
+	      	}
+	    }
+    }
+}
